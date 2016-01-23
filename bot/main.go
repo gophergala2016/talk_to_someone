a46e514f6b05379e1a10436d477800ef0ab5a5d8
Artem Nikitin <artemnikitin@artemnikitin.com>
2016-01-23 12:56:01 +0100

    Bot, work in progress

---
+++ bot/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/artemnikitin/talk_to_someone_client"
+)
+
+var (
+	server = flag.String("server", "localhost:8080/ws", "Server address")
+)
+
+func main() {
+	flag.Parse()
+	client.CreateConnection(*server, "BOT", "BOT")
+	log.Println("Client active =", client.IsActive())
+	for {
+		message := client.GetMessage()
+		log.Println("Message received =", message)
+		client.SendMessage("Mmm, you're so cool )")
+		log.Println("Message sended")
+	}
+	client.CloseConnection()
+}
